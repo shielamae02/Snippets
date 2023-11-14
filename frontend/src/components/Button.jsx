6fd6b411a59029aa41321cae159a8a96da864737
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-14 21:32:45 +0800

    Add button styling

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,46 @@
+export const Button = ({
+    children,
+    loading = false,
+    disabled,
+    className = "",
+    ...props
+}) => {
+    disabled = disabled || loading;
 
-const Button = () => {
     return (
-        <div>
-            Button
-        </div>
+        <button
+            disabled={disabled}
+            className={`bg-primary-light px-5 py-3 text-md text-white rounded-lg shadow-xl font-semibold  flex self-end items-center
+			${className} ${disabled
+                    ? "opacity-60 cursor-not-allowed"
+                    : "transform hover:-translate-y-px focus:shadow-sm focus:-translate-y-0"
+                }
+			`}
+            {...props}
+        >
+            {!disabled && !loading}
+            {disabled && !loading && (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 mr-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                    />
+                </svg>
+            )}
+            {loading && (
+                <div className="relative animate-spin w-6 h-6 rounded-full border-4 border-tertiary-light border-t-jetblack-light mr-2" />
+            )}
+            {children}
+        </button>
     );
-}
+};
 
 export default Button;
