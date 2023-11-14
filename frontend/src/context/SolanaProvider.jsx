0b66265d6c4bbbbef9c00e497e60cd17ee0fe76c
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-14 16:08:30 +0800

    Add tailwindcss package

---
+++ frontend/src/context/SolanaProvider.jsx
@@ -0,0 +1,9 @@
+const SolanaProviders = ({ children }) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+};
+
+export default SolanaProviders;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SolanaProviders from "./context/SolanaProvider";
+import App from "./App";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(
+  document.querySelector("#root")
+);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SolanaProviders>
+      <App />
+    </SolanaProviders>
+  </React.StrictMode>
+);
