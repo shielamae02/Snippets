c9459672882c24bce9f56db7819bdaa1a32b8912
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-14 21:27:00 +0800

    Add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./index.html"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      montserrat: ["Montserrat", "sans"]
+    },
+    extend: {
+      colors: {
+        "primary": {
+          "light": "#424874"
+        },
+        "secondary": {
+          "light": "#A6B1E1"
+        },
+        "tertiary": {
+          "light": "#DCD6F7"
+        },
+        "silver": {
+          "light": "#F4EEF6"
+        },
+        "jetblack": {
+          "light": "#2C3639"
+        }
+      }
+    },
   },
   plugins: [],
 }
