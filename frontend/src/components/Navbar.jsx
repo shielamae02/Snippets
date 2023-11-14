53ce36fe2ae9f3706ca928bfeb35b56b9ee87948
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-14 21:34:06 +0800

    Add navbar styling and props

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,53 @@
+import Button from "./Button"
 
-const Navbar = () => {
+const Navbar = (prop) => {
     return (
-        <div>
-            Navbar
-        </div>
-    );
+        <>
+            <div className="w-full fixed top-0 z-50 bg-[#F9FAFB] px-4 shadow-sm">
+                <div className="max-w-screen-2xl mx-auto py-4 justify-between flex items-center">
+                    <h1 className="font-montserrat text-4xl font-black text-black">
+                        Snippets
+                    </h1>
+                    <div className="flex gap-5">
+                        {prop.connected ? (
+                            <div className="flex items-center">
+                                <div className="flex justify-center items-center gap-2  rounded-lg">
+                                    <img src={prop.avatar} className="h-10 w-10 bg-jetblack-light rounded-full bg-tertiary-100" />
+                                    <p className="text-lg font-semibold text-primary-light">
+                                        {prop.username}
+                                    </p>
+                                </div>
+                                {prop.connected ? (
+                                    <Button
+                                        className="ml-3 mr-2"
+                                        onClick={prop.disconnect}>
+                                        Disconnect Wallet
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        className="ml-3 mr-2"
+                                        onClick={prop.initUser}>
+                                        Initialize User
+                                    </Button>
+                                )
+                                }
+                            </div>
+                        ) : (
+                            <Button
+                                loading={prop.connecting}
+                                className="w-auto"
+                                onClick={prop.onConnect}>
+                                Connect Wallet 🚀
+                            </Button>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar;
+
+
+
