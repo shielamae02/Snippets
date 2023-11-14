1aaca0f0b50185ed105b24c9f93452dae035f41e
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-14 18:26:38 +0800

    Add getPostById function

---
+++ frontend/src/context/functions/getPostById.js
@@ -0,0 +1,21 @@
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+
+export async function getPostById (postId, program) {
+    try {
+        const post = await program.account.postAccount.fetch(
+            new PublicKey(postId));
+        const userId = post.user.toString();
+
+        if(userId === SystemProgram.programId.toString()){
+            return;
+        }
+
+        return {
+            id: postId,
+            content: post.content,
+            userId
+        };
+    } catch (e) {
+        console.log(e.message);
+    }
+}
