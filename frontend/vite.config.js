83a3a00f9385dfc10d77c00e6007d438a4416101
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-18 02:56:23 +0800

    Update vite conifg for rollup plugin inject

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
-    react()
+    react(),
   ],
+  build:
+    isProduction
+      ? {
+        rollupOptions: {
+          plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+        },
+      }
+      : undefined,
   define: {
     'process.env.ANCHOR_BROWSER': true,
   },
