280ac9a9e98e4637712c337e67c0e1b2408bcb68
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-18 04:16:02 +0800

    Update avatar toggle function

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import Button from "./Button"
+import Button from "./Button";
+import Snippets from "../assets/Snippets.svg";
 
 const Navbar = (prop) => {
+
     return (
         <>
-            <div className="w-full fixed top-0 z-50 px-4 2xl:px-0 bg-[#F9FAFB] shadow-sm">
+            <div className="w-full fixed top-0 z-10 px-4 2xl:px-0 bg-[#F9FAFB] shadow-sm">
                 <div className="max-w-screen-2xl mx-auto py-4 justify-between flex items-center">
-                    <h1 className="font-montserrat text-4xl font-black text-black">
-                        Snippets
-                    </h1>
+                    <div className="flex gap-2 items-center">
+                        <img src={Snippets} className="h-8 stroke-slate-50" />
+                        <h1 className="font-montserrat text-4xl font-black text-jetblack">
+                            Snippets
+                        </h1>
+                    </div>
                     <div className="flex gap-5">
                         {
                             prop.connected ? (
@@ -17,24 +22,26 @@ const Navbar = (prop) => {
                                             <p className="text-lg font-semibold text-primary-light">
                                                 {prop.username}
                                             </p>
-                                            <img src={prop.avatar} className="h-10 w-10 bg-jetblack-light rounded-full bg-tertiary-100" />
+                                            <img
+                                                onClick={prop.toggleModal}
+                                                src={prop.avatar} className="h-10 w-10 cursor-pointer lg:cursor-none lg:pointer-events-none bg-jetblack-light rounded-full bg-tertiary-100" />
                                         </div>
                                     </div>
                                 ) : (
-                                        <Button
-                                            className="ml-3"
-                                            loading={prop.connecting}
-                                            onClick={prop.initUser}>
-                                            Initialize User 🔑
-                                        </Button>
+                                    <Button
+                                        className="ml-3"
+                                        loading={prop.connecting}
+                                        onClick={prop.initUser}>
+                                        Initialize User 🔑
+                                    </Button>
                                 )
-                            ): (
+                            ) : (
                                 <Button
-                                className="ml-3"
-                                loading={prop.connecting}
-                                onClick={prop.connect}>
+                                    className="ml-3"
+                                    loading={prop.connecting}
+                                    onClick={prop.connect}>
                                     Connect Wallet 🚀
-                            </Button>
+                                </Button>
                             )
                         }
                     </div>
