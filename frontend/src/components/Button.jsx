0272970e7c474db1ae612161f2d9e2aab7c799ab
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-17 14:34:21 +0800

    Remove button property

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -12,29 +12,13 @@ export const Button = ({
             disabled={disabled}
             className={`bg-primary-light px-5 py-3 flex items-center justify-center text-md text-white rounded-lg shadow-xl font-semibold  
 			${className} ${disabled
-                    ? "opacity-60 cursor-not-allowed"
+                    ? "opacity-80 cursor-not-allowed"
                     : "transform hover:-translate-y-px focus:shadow-sm focus:-translate-y-0"
                 }
 			`}
             {...props}
         >
             {!disabled && !loading}
-            {disabled && !loading && (
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 mr-1"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                    />
-                </svg>
-            )}
             {loading && (
                 <div className="relative animate-spin w-6 h-6 rounded-full border-4 border-tertiary-light border-t-jetblack-light mr-2" />
             )}
