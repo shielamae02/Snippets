4938c439d5639ed24bb992fabea48b5faa34e3c6
Shiela Mae Lepon <shiela.mlepon@gmail.com>
2023-11-18 14:37:06 +0800

    Add generate random letters function

--- frontend/src/functions/getRandomName.js
+++ frontend/src/functions/getRandomName.js
@@ -3,3 +3,15 @@ import nameGenerator from "@afuggini/namegenerator";
 export const getRandomName = () => {
     return nameGenerator(" ");
 };
+
+export const generateRandomLetters = (length) => {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let randomString = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * letters.length);
+        randomString += letters.charAt(randomIndex);
+    }
+
+    return randomString;
+};
